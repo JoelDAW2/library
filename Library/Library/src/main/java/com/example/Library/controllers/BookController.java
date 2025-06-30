739ced7428d712e1e2aff2456c1e46ba499a8dbb
Marc-0207 <a17marlamver@inspedralbes.cat>
2025-06-30 12:00:03 +0200

    Controlador
    Controlador con un helloWorld de prueba

--- Library/Library/src/main/java/com/example/Library/controllers/BookController.java
+++ Library/Library/src/main/java/com/example/Library/controllers/BookController.java
@@ -1,5 +1,14 @@
 package com.example.Library.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
 public class BookController {
+    @GetMapping("/helloWorld")
+    public String helloWorld() {
+        return "¡Hola desde Spring boot!";
+    }
     
 }
