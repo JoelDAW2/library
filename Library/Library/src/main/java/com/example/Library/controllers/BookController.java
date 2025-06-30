da80fa9fb66df91c61b9ce3995b2e8236d175aee
Marc-0207 <a17marlamver@inspedralbes.cat>
2025-06-30 13:28:32 +0200

    Controller, Repository, Service y Book
    Creación del controlador, el repositorio, servicio y clase Book

--- Library/Library/src/main/java/com/example/Library/controllers/BookController.java
+++ Library/Library/src/main/java/com/example/Library/controllers/BookController.java
@@ -1,11 +1,44 @@
 package com.example.Library.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.Library.models.Book;
+import com.example.Library.services.BookService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/*Para hacer test, ejecutar './mvnw spring-boot:run' dentro de la carpeta Library y ir a esta 
+URL: http://localhost:8080/helloWorld helloWorld puede cambiar si se edita el /helloWorld de @getMapping */
 
 @RestController
+@RequestMapping("/api/books")
 public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book){
+        return bookService.createBook(book);
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks(){
+        return bookService.getAllBooks();
+    }
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    public String postMethodName(@RequestBody String entity) {
+        
+        return entity;
+    }
     @GetMapping("/helloWorld")
     public String helloWorld() {
         return "¡Hola desde Spring boot!";
