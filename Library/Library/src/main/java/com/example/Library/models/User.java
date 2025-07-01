8bf79ea4ff71046004f169d4161e3490e8fd9620
MHF13 <41444696+MHF13@users.noreply.github.com>
2025-07-01 10:50:40 +0200

    Model: User

---
+++ Library/Library/src/main/java/com/example/Library/models/User.java
@@ -0,0 +1,18 @@
+package com.example.Library.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    public String name;
+    public String usersname;
+    public String password;
+}
