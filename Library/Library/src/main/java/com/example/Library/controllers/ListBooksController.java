a78399cc1d92ce2d7e16fa347619e1e69ee4d9fa
Marc-0207 <a17marlamver@inspedralbes.cat>
2025-07-07 11:47:06 +0200

    Mostrar libros usuario
    Ahora muestra los titulos de los libros que el usuario ha leído y los titulos de los libros que no ha leído(falta por pefeccionar esa parte)

--- Library/Library/src/main/java/com/example/Library/controllers/ListBooksController.java
+++ Library/Library/src/main/java/com/example/Library/controllers/ListBooksController.java
@@ -4,16 +4,25 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.Library.models.Book;
 import com.example.Library.services.ListBooksService;
 
 @RestController
-@RequestMapping("/api/login/books")
+@RequestMapping("/api/books")
 public class ListBooksController {
-    public List<Book> getAllBooksTitles(){
-        return ListBooksService.getAllBooksTitles();
+
+    @Autowired
+    private ListBooksService listBooksService;
+    
+    @GetMapping("/read/user/{userId}")
+    public List<String> findBooksReadByUser(@PathVariable Long userId) {
+        return listBooksService.findBooksReadByUser(userId);
+    }
+    @GetMapping("/noread/user/{userId}")
+    public List<String> findBooksNoReadByUser(@PathVariable Long userId){
+        return listBooksService.findBooksNoReadByUser(userId);
     }
 }
--- Library/Library/src/main/java/com/example/Library/services/ListBooksService.java
+++ Library/Library/src/main/java/com/example/Library/services/ListBooksService.java
@@ -3,13 +3,20 @@ package com.example.Library.services;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.Library.models.Book;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import com.example.Library.repositories.ListBooksRepository;
 
 @Service
 public class ListBooksService {
 
-    public List<Book> getAllBooksTitles(){
-        return ListBooksRepository.getAllBooksTitles();
+    @Autowired
+    private ListBooksRepository listBooksRepository;
+
+    public List<String> findBooksReadByUser(@PathVariable Long userId){
+        return listBooksRepository.findBooksReadByUser(userId);
+    }
+    public List<String> findBooksNoReadByUser(@PathVariable Long userId){
+        return listBooksRepository.findBooksNoReadByUser(userId);
     }
 }
