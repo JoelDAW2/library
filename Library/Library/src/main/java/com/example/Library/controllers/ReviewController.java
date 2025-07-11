828d4682a1156abbf16bff8ac51bdedb0b040183
MHF13 <41444696+MHF13@users.noreply.github.com>
2025-07-11 13:42:06 +0200

    Muestras ordenadas ejemplo

--- Library/Library/src/main/java/com/example/Library/controllers/ReviewController.java
+++ Library/Library/src/main/java/com/example/Library/controllers/ReviewController.java
@@ -1,8 +1,10 @@
 package com.example.Library.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +49,47 @@ public class ReviewController{
 
     @GetMapping("/BookReviews_{bookName}")
     public List<String> BookReviews(@PathVariable String bookName){
-        return reviewService.BookReviews(bookName);
+        List<String> a = reviewService.BookReviews(bookName);
+
+        List<String> ret = new ArrayList<>();
+
+        for (int i = 0; i < a.size(); i++) {
+            int comasCount = 0;
+            String Bonus ="";
+            Bonus += "Username: ";
+
+            String b= a.get(i);
+
+            for (int j = 0; j < b.length(); j++) {
+                if (b.charAt(j) == ',' && comasCount <= 3) {
+                    switch (comasCount) {
+                        case 0:
+                        ret.add(Bonus);
+                            Bonus = "Title: ";
+                        break;
+                        case 1:
+                        ret.add(Bonus);
+                            Bonus = "Score: ";
+                        break;
+                        case 2: 
+                        ret.add(Bonus);
+                            Bonus = "Review: ";
+                        break;
+                    }
+                    comasCount++;
+                }
+                else{
+                    Bonus += b.charAt(j);
+                }
+            }
+            ret.add(Bonus);
+            if (i < a.size()-1) {
+                Bonus = "-----------------------";
+                ret.add(Bonus);
+            }
+        }
+
+        return ret;
+
     }
 }
