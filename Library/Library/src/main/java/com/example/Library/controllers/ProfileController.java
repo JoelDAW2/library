dc6fe87376a808e369b7a0835e1a4b0d2c439055
Marc-0207 <a17marlamver@inspedralbes.cat>
2025-07-11 14:17:02 +0200

    Perfil mas ordenado
    Perfil de usuario y perfiles de usuarios más ordenados y limpios

--- Library/Library/src/main/java/com/example/Library/controllers/ProfileController.java
+++ Library/Library/src/main/java/com/example/Library/controllers/ProfileController.java
@@ -1,5 +1,7 @@
 package com.example.Library.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +20,50 @@ public class ProfileController {
     
     @GetMapping("/{userId}/allreviews")
     public List<String> showUsers(@PathVariable Long userId) {
-        return listProfileService.showUsers(userId);
+         List<String> a = listProfileService.showUsers(userId);
+
+        List<String> pauta = Arrays.asList("Username","Title", "Score","Review");
+
+        return Ordenar(a, pauta);
     }
     
     @GetMapping("/{userId}")
     public List<String> showProfile(@PathVariable Long userId){
-        return listProfileService.showProfile(userId);
+        List<String> a = listProfileService.showProfile(userId);
+
+        List<String> pauta = Arrays.asList( "Title", "Score","Review");
+
+        return Ordenar(a, pauta);
+    }
+    public List<String> Ordenar(List<String> datos, List<String> pauta){
+
+        List<String> ret = new ArrayList<>();
+
+        for (int i = 0; i < datos.size(); i++) {
+            int comasCount = 0;
+            String Bonus ="";
+            Bonus += pauta.get(comasCount) + ": ";
+            
+            String b= datos.get(i);
+
+            for (int j = 0; j < b.length(); j++) {
+                if (b.charAt(j) == ',' && comasCount < pauta.size()-1) {
+                    ret.add(Bonus);
+                    Bonus = pauta.get(comasCount+1)+ ": ";
+                    comasCount++;
+                }
+                else{
+                    Bonus += b.charAt(j);
+                }
+            }
+            ret.add(Bonus);
+            if (i < datos.size()-1) {
+                Bonus = "-----------------------";
+                ret.add(Bonus);
+            }
+        }
+
+        return ret;
+
     }
 }
