3522b1d5d55dc9d41eb32b9701b5a61b396d35fd
MHF13 <41444696+MHF13@users.noreply.github.com>
2025-07-11 14:09:04 +0200

    Automatizacion de Ordenar

--- Library/Library/src/main/java/com/example/Library/controllers/ReviewController.java
+++ Library/Library/src/main/java/com/example/Library/controllers/ReviewController.java
@@ -1,6 +1,7 @@
 package com.example.Library.controllers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,27 +30,133 @@ public class ReviewController{
 
     @GetMapping
     public List<String> showReview(){
-        return reviewService.showReview();
+        List<String> a = reviewService.showReview();
+
+        List<String> ret = new ArrayList<>();
+
+        for (int i = 0; i < a.size(); i++) {
+            int comasCount = 0;
+            String Bonus ="";
+            Bonus += "Username: ";
+
+            String b= a.get(i);
+
+            for (int j = 0; j < b.length(); j++) {
+                if (b.charAt(j) == ',' && comasCount <= 3) {
+                    switch (comasCount) {
+                        case 0:
+                        ret.add(Bonus);
+                            Bonus = "Title: ";
+                        break;
+                        case 1:
+                        ret.add(Bonus);
+                            Bonus = "Score: ";
+                        break;
+                        case 2: 
+                        ret.add(Bonus);
+                            Bonus = "Review: ";
+                        break;
+                    }
+                    comasCount++;
+                }
+                else{
+                    Bonus += b.charAt(j);
+                }
+            }
+            ret.add(Bonus);
+            if (i < a.size()-1) {
+                Bonus = "-----------------------";
+                ret.add(Bonus);
+            }
+        }
+
+        return ret;
     }
 
     @GetMapping("/orderbyAVGScore")
-        public List<String> orderbyScore(){
-        return reviewService.orderbyScore();
+    public List<String> orderbyScore(){
+
+        List<String> a = reviewService.orderbyScore();
+
+        List<String> ret = new ArrayList<>();
+
+        for (int i = 0; i < a.size(); i++) {
+            int comasCount = 0;
+            String Bonus ="";
+            Bonus += "Title: ";
+
+            String b= a.get(i);
+
+            for (int j = 0; j < b.length(); j++) {
+                if (b.charAt(j) == ',' && comasCount <= 1) {
+                    switch (comasCount) {
+                        case 0:
+                        ret.add(Bonus);
+                            Bonus = "Score: ";
+                        break;
+                    }
+                    comasCount++;
+                }
+                else{
+                    Bonus += b.charAt(j);
+                }
+            }
+            ret.add(Bonus);
+            if (i < a.size()-1) {
+                Bonus = "-----------------------";
+                ret.add(Bonus);
+            }
+        }
+
+        return ret;
+        
     }
 
     @GetMapping("/IdUserReviews_{userId}")
         public List<String> IdUserReviews(@PathVariable Long userId){
-        return reviewService.IdUserReviews(userId);
+        List<String> a = reviewService.IdUserReviews(userId);
+
+                List<String> ret = new ArrayList<>();
+
+        for (int i = 0; i < a.size(); i++) {
+            int comasCount = 0;
+            String Bonus ="";
+            Bonus += "Title: ";
+
+            String b= a.get(i);
+
+            for (int j = 0; j < b.length(); j++) {
+                if (b.charAt(j) == ',' && comasCount <= 2) {
+                    switch (comasCount) {
+                        case 0:
+                        ret.add(Bonus);
+                            Bonus = "Score: ";
+                        break;
+                        case 1: 
+                        ret.add(Bonus);
+                            Bonus = "Review: ";
+                        break;
+                    }
+                    comasCount++;
+                }
+                else{
+                    Bonus += b.charAt(j);
+                }
+            }
+            ret.add(Bonus);
+            if (i < a.size()-1) {
+                Bonus = "-----------------------";
+                ret.add(Bonus);
+            }
+        }
+
+        return ret;
+        
     }
 
     @GetMapping("/UserNameReviews_{username}")
     public List<String> UserNameReviews(@PathVariable String username){
-        return reviewService.UserNameReviews(username);
-    }
-
-    @GetMapping("/BookReviews_{bookName}")
-    public List<String> BookReviews(@PathVariable String bookName){
-        List<String> a = reviewService.BookReviews(bookName);
+        List<String> a = reviewService.UserNameReviews(username);
 
         List<String> ret = new ArrayList<>();
 
@@ -91,5 +198,49 @@ public class ReviewController{
 
         return ret;
 
+
     }
+
+    @GetMapping("/BookReviews_{bookName}")
+    public List<String> BookReviews(@PathVariable String bookName){
+        List<String> a = reviewService.BookReviews(bookName);
+
+        List<String> pauta = Arrays.asList("Username", "Title", "Score","Review");
+
+        return Ordenar(a, pauta);
+
+    }
+
+    public List<String> Ordenar(List<String> datos, List<String> pauta){
+
+        List<String> ret = new ArrayList<>();
+
+        for (int i = 0; i < datos.size(); i++) {
+            int comasCount = 0;
+            String Bonus ="";
+            Bonus += pauta.get(comasCount) + ": ";
+            
+            String b= datos.get(i);
+
+            for (int j = 0; j < b.length(); j++) {
+                if (b.charAt(j) == ',' && comasCount < pauta.size()-1) {
+                    ret.add(Bonus);
+                    Bonus = pauta.get(comasCount+1)+ ": ";
+                    comasCount++;
+                }
+                else{
+                    Bonus += b.charAt(j);
+                }
+            }
+            ret.add(Bonus);
+            if (i < datos.size()-1) {
+                Bonus = "-----------------------";
+                ret.add(Bonus);
+            }
+        }
+
+        return ret;
+
+    }
+
 }
--- Library/Library/src/main/java/com/example/Library/repositories/ReviewRepository.java
+++ Library/Library/src/main/java/com/example/Library/repositories/ReviewRepository.java
@@ -11,7 +11,7 @@ import com.example.Library.models.Review;
 
 @Repository
     public interface ReviewRepository extends JpaRepository<Review, Long>{
-        @Query("SELECT u.username, b.title, r.comment, r.score FROM User u JOIN Review r ON u.id = r.userId join Book b on b.id = r.bookId")
+        @Query("SELECT u.username, b.title, r.score, r.comment  FROM User u JOIN Review r ON u.id = r.userId join Book b on b.id = r.bookId")
         List<String> showReview();
         
         @Query("SELECT b.title, AVG(r.score) FROM Review r inner join Book b ON b.id = r.bookId group by b.title order by AVG(r.score) desc")
